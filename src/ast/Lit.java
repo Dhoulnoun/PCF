052f91195a5abacf763fe10ba72c330e69431cae
victorbarreteau <victor.barreteau@etu.univ-nantes.fr>
2023-01-12 11:31:52 +0100

    interp lit

--- src/ast/Lit.java
+++ src/ast/Lit.java
@@ -1,9 +1,17 @@
 package ast;
 
+import interp.Env;
+import interp.Value;
+
 public class Lit extends Term {
     public int value;
 
     public Lit(int value) {
         this.value = value;
+    }
+
+    @Override
+    public Value interp(Env e) throws Exception {
+        throw new Exception("Not implemented");
     }
 }
