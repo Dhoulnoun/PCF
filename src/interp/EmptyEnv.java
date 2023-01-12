63a01d8add7f47babb760608fae640f42b9f45b0
Younes <younes.ghoniem@gmail.com>
2023-01-12 12:06:29 +0100

    Interp Bleu

--- src/interp/EmptyEnv.java
+++ src/interp/EmptyEnv.java
@@ -1,9 +1,35 @@
 package interp;
 
+import java.util.Optional;
+
 public class EmptyEnv extends Env {
 
     public EmptyEnv() {
 
     }
 
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public Binding last() {
+        throw new RuntimeException("Empty environment has no last binding");
+    }
+
+    @Override
+    public Env previous() {
+        throw new RuntimeException("Empty environment has no previous environment");
+    }
+
+    @Override
+    public Env add(String id, Value value) {
+        return new NonEmptyEnv(new Binding(id, value), this);
+    }
+
+    @Override
+    public Optional<Value> lookup(String id) {
+        return Optional.empty();
+    }
 }
